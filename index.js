a49d0073279878851642b0ad9f3981c9d3ee88a9
agathaemm <agathaemm@gmail.com>
2019-01-27 19:22:19 -0200

    Adiciona o key press

--- index.js
+++ index.js
@@ -2,11 +2,30 @@ const cli = require('./cli');
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const robot = require("kbm-robot");
+robot.startJar();
+
+const keys = {
+  'green': 'a',
+  'red': 's',
+  'yellow': 'j',
+  'blue': 'k',
+  'orange': 'l'
+}
 
 io.on('connection', function(socket){
-  console.info('a user connected');
-  socket.on('keypressed', data => {
+  console.log('Conta acessada.');
+  socket.on('keypressed', dados => {
+    let data = dados.data;
     console.log(data);
+    if ( data['type'] == 'start' ) {
+      console.log(keys[data['key']]);
+      robot.press(keys[data['key']]).go();
+    }
+    if ( data['type'] == 'end' ) {
+      console.log(keys[data['key']]);
+      robot.release(keys[data['key']]).go();
+    }
   });
 });
 
